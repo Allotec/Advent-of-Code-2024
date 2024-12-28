d7b686825b67d6846bdcf061adea77e7a162555a
Matthew Champagne <champagne7103@gmail.com>
2024-12-27 22:24:11 -0500

    Finished day_3 part 1

--- day_3/part1.rs
+++ day_3/part1.rs
@@ -1,3 +1,24 @@
+use regex::Regex;
+
 pub fn part1(input: &[&str]) -> i32 {
-    todo!()
+    let hay = concat_input(input);
+    let re = Regex::new(r"mul\((?<num1>[0-9]+),(?<num2>[0-9]+)\)").unwrap();
+
+    re.captures_iter(hay.as_str())
+        .map(|caps| {
+            let num1 = caps.name("num1").unwrap().as_str().parse::<i32>();
+            let num2 = caps.name("num2").unwrap().as_str().parse::<i32>();
+            num1.unwrap() * num2.unwrap()
+        })
+        .sum::<i32>()
+}
+
+fn concat_input(input: &[&str]) -> String {
+    let mut concated = String::new();
+
+    input.iter().for_each(|s| {
+        concated.push_str(s);
+    });
+
+    concated
 }
